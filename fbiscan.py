26c7ecedde96473c98846bf81b7db8a1d5e92bab
Christopher Lycan <96343159+777388@users.noreply.github.com>
2023-10-23 23:59:06 -0700

    Update fbiscan.py

--- fbiscan.py
+++ fbiscan.py
@@ -2,7 +2,7 @@ import sys
 import re
 with open("namefile FBI.mhtml", "r") as f:
     for line in f:
-        if re.search(sys.argv[1], line):
+        if re.search(sys.argv[1].lower(), line.lower()):
             print(line)
 
 print("end")
